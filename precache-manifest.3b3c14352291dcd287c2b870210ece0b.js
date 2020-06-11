6a973174e074702fff074ff9d3eae97394262d85
Icaruk <adri-rk@hotmail.com>
2020-06-11 22:45:46 +0200

    Updates

---
+++ precache-manifest.3b3c14352291dcd287c2b870210ece0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b33624cdd3760b61383e",
+    "url": "/icaruk.dev/static/css/main.97657b08.chunk.css"
+  },
+  {
+    "revision": "b33624cdd3760b61383e",
+    "url": "/icaruk.dev/static/js/main.b33624cd.chunk.js"
+  },
+  {
+    "revision": "2261685204a8e4ece695",
+    "url": "/icaruk.dev/static/js/1.22616852.chunk.js"
+  },
+  {
+    "revision": "cad7d428ea8c13f80a18",
+    "url": "/icaruk.dev/static/js/runtime~main.cad7d428.js"
+  },
+  {
+    "revision": "91c6fda79c0210861c04d6037e6242cd",
+    "url": "/icaruk.dev/index.html"
+  }
+];
