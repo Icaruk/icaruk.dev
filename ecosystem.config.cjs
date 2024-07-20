e4f60b3757054fe36cfcd9108c7b858d6bee7ada
Icaruk <adri-rk@hotmail.com>
2024-07-20 19:23:00 +0200

    a

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,14 +2,24 @@ module.exports = {
 	apps: [
 		{
 			name: "icarukdev",
-			script: "pm2",
+			script: "serve",
 
-			args: "serve ./dist --port 8200",
 			log_date_format: "DD/MM/YYYY HH:mm:ss Z",
 
-			env_production: {
+			instances: 1,
+			exec_mode: "fork",
+			watch: true,
+
+			exp_backoff_restart_delay: 100,
+			ignore_watch: ["node_modules", ".github"],
+
+			env: {
 				NODE_ENV: "production",
 				TZ: "Europe/Madrid",
+				PM2_SERVE_PATH: "dist",
+				PM2_SERVE_PORT: 8200,
+				PM2_SERVE_SPA: "false",
+				PM2_SERVE_HOMEPAGE: "/index.html",
 			},
 		},
 	],
