45101d6fa9fa19f2f26c0e2d73e405b0f0ad749f
Icaruk <adri-rk@hotmail.com>
2024-07-20 19:28:26 +0200

    build: spa true

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -18,7 +18,7 @@ module.exports = {
 				TZ: "Europe/Madrid",
 				PM2_SERVE_PATH: "dist",
 				PM2_SERVE_PORT: 8200,
-				PM2_SERVE_SPA: "false",
+				PM2_SERVE_SPA: true,
 				PM2_SERVE_HOMEPAGE: "/index.html",
 			},
 		},
