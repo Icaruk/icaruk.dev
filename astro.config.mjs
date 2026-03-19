d1e508b6f10d6a7fba587538b4a6f0cdf981a44b
Icaruk <adri-rk@hotmail.com>
2026-03-19 23:39:21 +0100

    feat: upgrade astro 5 to 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
+	site: "https://icaruk.dev",
 
 	integrations: [
 		starlight({
