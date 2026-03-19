3717c265e4c1c167fae60606f632a38ae42ef48f
Icaruk <adri-rk@hotmail.com>
2026-03-20 00:55:45 +0100

    refactor: configuration and styles. Add view transitions, update Head component, and clean up .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,35 +5,41 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
 	output: "static",
 	site: "https://icaruk.dev",
+	prefetch: {
+		prefixes: [
+			"/"
+		]
+	},
 
-	integrations: [
-		starlight({
-			title: 'Adrian "Icaruk"',
-			social: [
-				{
-					icon: "github",
-					label: "Github",
-					href: "https://github.com/icaruk",
-				}
-			],
-			sidebar: [
-				{
-					label: "Home",
-					link: "/",
-				},
-				{
-					label: "About me",
-					autogenerate: { directory: "about" },
-				},
-				{
-					label: "Work",
-					autogenerate: { directory: "work" },
-				},
-			],
-			customCss: ["./src/assets/landing.css"],
-			editLink: {
-				baseUrl: "https://github.com/icaruk/icaruk.dev/edit/main/",
+	integrations: [starlight({
+		title: 'Adrian "Icaruk"',
+		social: [
+			{
+				icon: "github",
+				label: "Github",
+				href: "https://github.com/icaruk",
+			}
+		],
+		sidebar: [
+			{
+				label: "Home",
+				link: "/",
 			},
-		}),
-	],
+			{
+				label: "About me",
+				autogenerate: { directory: "about" },
+			},
+			{
+				label: "Work",
+				autogenerate: { directory: "work" },
+			},
+		],
+		customCss: ["./src/assets/landing.css"],
+		editLink: {
+			baseUrl: "https://github.com/icaruk/icaruk.dev/edit/main/",
+		},
+		components: {
+			Head: './src/components/Head.astro',
+		},
+	})],
 });
